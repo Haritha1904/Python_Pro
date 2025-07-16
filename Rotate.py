5d15bc465066fcc786ef6fe57c67c34fbe04b77b
Haritha1904 <rejeti.haritha@gmail.com>
2025-07-16 06:07:48 +0530

    Rotata 2D matrix

---
+++ Rotate.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+point = np.array([3,5])
+theta = np.radians(90)
+
+rotation_matrix = np.array([
+    [np.cos(theta), -np.sin(theta)],
+    [np.sin(theta), np.cos(theta)]
+])
+
+new_point = rotation_matrix@(point)
+print("Rotated point ",new_point)
